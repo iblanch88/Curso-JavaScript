8071be9944d80a83c23133c123a4aae9554ae790
iblanch88 <ignacioblanch.ib@gmail.com>
2021-11-08 19:49:48 -0300

    ok

---
+++ Clase02/main.js
@@ -0,0 +1,9 @@
+let nombre= prompt ('Ingresa tu nombre')
+let edad= parseInt (prompt('Ingresa tu edad'))
+
+if(edad >= 18){
+    alert(nombre + ": Sos mayor de edad")
+}
+else {
+    alert (nombre + ": Sos menor de edad")
+}
---
+++ Clase03/js/main.js
@@ -0,0 +1,46 @@
+let texto = prompt("INGRESE UNA FRUTA:");
+
+while(texto != "FIN") {
+    switch(texto) {
+
+        case "MANZANA" :
+            console.log("El kilo de Manzanas esta $150")
+        break;
+
+        case "BANANA" :
+             console.log("Promoción 2 Kg de $200")
+        break;
+
+        case "PERA":
+            console.log("El kilo de Pera está $120")
+        break;
+
+        case "SANDIA" :
+            console.log("El kilo de Sandía está $100")
+        break;
+
+        case "FRUTILLA" :
+            console.log("SUPER PROMO: 2 KG de FRUTILLA está $450")
+        break;
+        
+        default:
+            console.log ("NO TENEMOS" + texto)
+            break;
+        
+    }
+
+    let texto = prompt("INGRESE OTRA FRUTA:")
+
+
+}
+ console.log("GRACIAS POR SU VISITA")
+
+
+/*let numero = parseInt (prompt("Ingrese un numero:"))
+
+for (let i=0; i<=numero; i++ ){
+    let suma = numero + i
+    console.log(suma)
+}*/
+
+  
